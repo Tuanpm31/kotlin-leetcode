b69749a9f41a6a4e321aaa18be970af269018db4
Pham Minh Tuan <tuan.pm1@teko.vn>
2021-01-18 17:45:59 +0700

    TwoSum

--- src/main/kotlin/TwoSum.kt
+++ src/main/kotlin/TwoSum.kt
@@ -1,25 +1,20 @@
 class TwoSum {
-//    fun twoSum(nums: IntArray, target: Int): IntArray {
-//        val l = 0
-//        val r = nums.size - 1
-//        while (l <= r) {
-//            when {
-//                (nums[l] + nums[r]) == target -> {
-//                    return intArrayOf(l, r)
-//                }
-//                (nums[l] + nums[r]) > target -> {
-//                    l + 1
-//                }
-//                else -> {
-//                    r - 1
-//                }
-//            }
-//        }
-//        return intArrayOf(0, 0)
-//    }
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val hashMap = HashMap<Int, Int>()
+        nums.forEachIndexed { index, value ->
+            hashMap[value] = index
+        }
+        nums.forEachIndexed { index, i ->
+            val diff = target - i
+            hashMap[diff]?.let {
+                if (it != index) return intArrayOf(index, it)
+            }
+        }
+        return intArrayOf(0, 0)
+    }
 }
 
 fun main() {
     val s = TwoSum()
-    println(s.twoSum(intArrayOf(2,7,11,15), 9))
+    println(s.twoSum(intArrayOf(2,7,11,15), 9).toList())
 }
