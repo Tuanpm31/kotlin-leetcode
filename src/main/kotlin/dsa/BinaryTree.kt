ddc998177bf93b9ff6f8d72d9eddb56f25675d67
Minh Tuan <phmtuan313@gmail.com>
2021-01-27 23:38:12 +0700

    Binary tree updated

--- src/main/kotlin/dsa/BinaryTree.kt
+++ src/main/kotlin/dsa/BinaryTree.kt
@@ -5,25 +5,55 @@ class BinaryNode(
     var left: BinaryNode? = null,
     var right: BinaryNode? = null
 ) {
-    fun find(value: Int): BinaryNode? = when {
-        key > value -> left?.find(value)
-        key < value -> right?.find(value)
-        else -> this
-    }
-
     fun insert(value: Int) {
-        if (value < key) {
+        if (value <= key) {
             if (left == null) {
                 left = BinaryNode(value)
             } else {
                 left!!.insert(value)
             }
-        } else if (value > key) {
+        } else {
             if (right == null) {
                 right = BinaryNode(value)
             } else {
-                left!!.insert(value)
+                right!!.insert(value)
             }
         }
     }
+
+    fun contains(value: Int): Boolean {
+        if (key == value) return true
+        if (value < key) {
+            if (left == null) {
+                return false
+            } else {
+                return left!!.contains(value)
+            }
+        } else {
+            if (right == null) {
+                return false
+            } else {
+                return right!!.contains(value)
+            }
+        }
+    }
+
+    fun printInOrder() {
+        if (left != null) {
+            left!!.printInOrder()
+        }
+        println(key)
+        if (right != null) {
+            right!!.printInOrder()
+        }
+    }
+}
+
+fun main() {
+    val tree = BinaryNode(3)
+    tree.insert(2)
+    tree.insert(1)
+    tree.insert(4)
+    tree.insert(6)
+    tree.printInOrder()
 }
